e592ffe48292e722feefbbc8944c3146121d61af
Harshit Rautela <harshitsrautela@gmail.com>
2024-08-09 17:33:51 +0530

    6th commit

--- client/src/components/AllBlogs.jsx
+++ client/src/components/AllBlogs.jsx
@@ -1,8 +1,6 @@
-// src/pages/AllBlogs.js
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getUserBlogs } from "../services/BlogsApi";
-import axios from "axios";
 import BackButton from "./Buttons/BackButton";
 
 const AllBlogs = () => {
@@ -19,7 +17,6 @@ const AllBlogs = () => {
         }
         const userBlogs = await getUserBlogs(token);
         setBlogs(userBlogs);
-        //setBlogs(response.data);
       } catch (error) {
         console.error("Failed to fetch blogs:", error);
         navigate("/login");
@@ -27,6 +24,7 @@ const AllBlogs = () => {
     };
     fetchBlogs();
   }, [navigate]);
+
   const backgroundImageUrl =
     "https://images.unsplash.com/photo-1499750310107-5fef28a66643?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8YmxvZ2dpbmd8ZW58MHx8MHx8fDA%3D";
 
@@ -38,16 +36,18 @@ const AllBlogs = () => {
       <div className="absolute inset-0 bg-black bg-opacity-50"></div>
 
       <div className="relative container mx-auto py-12 px-6 text-white">
-        <h1 className="text-4xl font-bold text-center mb-8">All Blogs</h1>
-        <BackButton/>
-        <br/>
+        <h1 className="text-4xl font-bold text-center mb-8 animate__animated animate__fadeIn">
+          All Blogs
+        </h1>
+        <BackButton />
+        <br />
 
         <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
           {blogs.length > 0 ? (
             blogs.map((blog) => (
               <div
                 key={blog._id}
-                className="bg-white rounded-lg shadow-md hover:shadow-lg transform transition-all duration-300 hover:-translate-y-1"
+                className="bg-white rounded-lg shadow-md hover:shadow-lg transform transition-all duration-300 hover:-translate-y-2 hover:scale-105"
               >
                 <div className="p-6 text-gray-800">
                   <h2 className="text-xl font-semibold mb-2">{blog.title}</h2>
@@ -57,7 +57,7 @@ const AllBlogs = () => {
                   <div className="text-right">
                     <button
                       onClick={() => navigate(`/blog/${blog._id}`)}
-                      className="text-white bg-indigo-600 hover:bg-red-600 font-semibold py-2 px-4 rounded-full transition-all duration-300 transform hover:scale-105 hover:shadow-lg"
+                      className="text-white bg-indigo-600 hover:bg-indigo-700 font-semibold py-2 px-4 rounded-full transition-all duration-300 transform hover:scale-105 hover:shadow-lg"
                     >
                       Read More
                     </button>
