7a6c9403a32f05590799343af9da641d00a32cdf
Harshit Rautela <harshitsrautela@gmail.com>
2024-08-13 20:21:58 +0530

    msg

--- client/src/services/BlogsApi.js
+++ client/src/services/BlogsApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/blogs',
+  baseURL: 'https://blogging-ten-nu.vercel.app/api/blogs',
   headers: {
     'Content-Type': 'application/json',
   },
